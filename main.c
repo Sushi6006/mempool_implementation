65e76da1e930c4ab071a695d549431c7071bebfb
SushiCat <55708782+Sushi6006@users.noreply.github.com>
2022-10-26 16:57:51 +0800

    Update main.c

--- main.c
+++ main.c
@@ -6,9 +6,28 @@
 int main(void) {
 
     s_mempool_ctrl g_mempool_ctrl_info;
-
-    // TODO: testings: using the mempool
     mempool_init(&g_mempool_ctrl_info);
+    mempool_print(&g_mempool_ctrl_info);
+    VOS_VOID* memory_allocated[20];
+
+    // try to allocate to max and more
+    printf("ALLOCATING 20 BLOCKS OF MEMORY...\n");
+    for (int i = 0; i < 20; i++) {
+        VOS_VOID* p_ret = mempool_alloc(&g_mempool_ctrl_info, 20);
+        memory_allocated[i] = p_ret;
+        printf("memory allocated #%02d: %p\n", i, p_ret);
+    }
+    printf("\n\n");
+
+    mempool_print(&g_mempool_ctrl_info);
+
+    // free some of the memory
+    printf("FREEING 5 BLOCKS OF MEMORY...\n\n");
+    for (int i = 0; i < 5; i++) {
+        mempool_free(&(g_mempool_ctrl_info), memory_allocated[i]);
+    }
+
+    mempool_print(&g_mempool_ctrl_info);
 
     return 0;
 
