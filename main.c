c82f78d420bed89bfc1a5b6bdb08d36e77b30ffa
SushiCat <55708782+Sushi6006@users.noreply.github.com>
2022-10-26 22:16:54 +0800

    Update main.c

--- main.c
+++ main.c
@@ -29,6 +29,47 @@ int main(void) {
 
     mempool_print(&g_mempool_ctrl_info);
 
+    // try to allocate to max and more
+    printf("ALLOCATING 3 BLOCKS OF MEMORY...\n");
+    for (int i = 0; i < 3; i++) {
+        VOS_VOID* p_ret = mempool_alloc(&g_mempool_ctrl_info, 20);
+        memory_allocated[i] = p_ret;
+        printf("memory allocated #%02d: %p\n", i, p_ret);
+    }
+    printf("\n\n");
+
+    mempool_print(&g_mempool_ctrl_info);
+
+    // free some of the memory
+    printf("FREEING 7 BLOCKS OF MEMORY...\n\n");
+    for (int i = 0; i < 7; i++) {
+        mempool_free(&(g_mempool_ctrl_info), memory_allocated[i]);
+    }
+
+    mempool_print(&g_mempool_ctrl_info);
+
+    // try to allocate to max and more
+    printf("ALLOCATING 8 BLOCKS OF MEMORY...\n");
+    for (int i = 0; i < 8; i++) {
+        VOS_VOID* p_ret = mempool_alloc(&g_mempool_ctrl_info, 20);
+        memory_allocated[i] = p_ret;
+        printf("memory allocated #%02d: %p\n", i, p_ret);
+    }
+    printf("\n\n");
+
+    mempool_print(&g_mempool_ctrl_info);
+
+    // try to allocate to max and more
+    printf("ALLOCATING 20 BLOCKS OF MEMORY...\n");
+    for (int i = 0; i < 20; i++) {
+        VOS_VOID* p_ret = mempool_alloc(&g_mempool_ctrl_info, 20);
+        memory_allocated[i] = p_ret;
+        printf("memory allocated #%02d: %p\n", i, p_ret);
+    }
+    printf("\n\n");
+
+    mempool_print(&g_mempool_ctrl_info);
+
     return 0;
 
 }
